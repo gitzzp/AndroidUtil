2639b595fc16b9135367fa0ab54e920cffe93baf
gitzzp <429698984@qq.com>
2016-11-03 19:47:53 +0800

    Initial commit

---
+++ testlib/src/main/java/com/a9_lin/www/testlib/TestClass.java
@@ -0,0 +1,12 @@
+package com.a9_lin.www.testlib;
+
+/**
+ * Created by macmini on 16/11/3.
+ */
+
+public class TestClass {
+
+    public static void testMethod(){
+        System.out.println("测试引用");
+    }
+}
